4a12cb99cc28e6bb8daf8fad4486b4f1f2832b91
Kaleb014 <kaleb.d.douglas@gmail.com>
2025-02-15 15:00:56 -0600

    fin
    fin
    
    fixed decimals

--- Calculator/ViewModel/MainWindowViewModel.cs
+++ Calculator/ViewModel/MainWindowViewModel.cs
@@ -107,86 +107,90 @@ namespace Calculator.ViewModel
 					bool resolve = false;
 					counter++;
 
+					if(c != '+' && c != '-' && c != '.' && !char.IsDigit(c))
+					{
+						inputvalue = "Invalid input";
+						return;
+					}
+
 					//If we have multiple decimals, we skip the rest of the values until finding an operator or end of string
 					if (skipValues)
 					{
 						if (c == '+' || c == '-')
 						{
+							lastChar = c;
 							operatorChar = c;
 							skipValues = false;
-							resolve = true;
 						}
-						else if (counter == stringLength)
-						{
-							updateInputValue = true;
-							skipValues = false;
-							resolve = true;
-						}
-						else
+						else if (counter != stringLength)
 						{
 							continue;
 						}
 					}
 
-					//If we have an operator, we are building the right expression
-					if (operatorChar != ' ')
+					if (!skipValues)
 					{
-						if (c != '+' && c != '-')
+						//If we have an operator, we are building the right expression
+						if (operatorChar != ' ')
 						{
-							if (c == '.' && rightExpressionStr.Contains('.'))
+							if (c != '+' && c != '-')
 							{
-								skipValues = true;
-								continue;
-							}
+								if (c == '.' && rightExpressionStr.Contains('.'))
+								{
+									skipValues = true;
+									continue;
+								}
 
-							lastChar = c;
-							rightExpressionStr += c;
-						}
-						else if (lastChar == '+' || lastChar == '-')
-						{
-							lastChar = c;
-							operatorChar = c;
-						}
-						else
-						{
-							lastChar = c;
-							operatorChar = c;
-							resolve = true;
-						}
-					}
-					//We are building the left expression
-					else
-					{
-						if (c != '+' && c != '-')
-						{
-							if (c == '.' && leftExpressionStr.Contains('.'))
-							{
-								skipValues = true;
-								continue;
-							}
-
-							lastChar = c;
-							leftExpressionStr += c;
-						}
-						else
-						{
-							if (leftExpressionStr.Length == 0)
-							{
 								lastChar = c;
-								leftExpressionStr += c;
+								rightExpressionStr += c;
 							}
 							else if (lastChar == '+' || lastChar == '-')
 							{
 								lastChar = c;
-								leftExpressionStr = c.ToString();
+								operatorChar = c;
 							}
 							else
 							{
 								lastChar = c;
 								operatorChar = c;
+								resolve = true;
+							}
+						}
+						//We are building the left expression
+						else
+						{
+							if (c != '+' && c != '-')
+							{
+								if (c == '.' && leftExpressionStr.Contains('.'))
+								{
+									skipValues = true;
+									continue;
+								}
+
+								lastChar = c;
+								leftExpressionStr += c;
+							}
+							else
+							{
+								if (leftExpressionStr.Length == 0)
+								{
+									lastChar = c;
+									leftExpressionStr += c;
+								}
+								else if (lastChar == '+' || lastChar == '-')
+								{
+									lastChar = c;
+									leftExpressionStr = c.ToString();
+								}
+								else
+								{
+									lastChar = c;
+									operatorChar = c;
+								}
 							}
 						}
 					}
+					
 
 					//If we are at the end of the string, we need to resolve the expression
 					if (counter == stringLength)
