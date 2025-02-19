9cf64f0f0278efbf41967b936bc48f0701a98cb7
Kaleb014 <kaleb.d.douglas@gmail.com>
2025-02-18 21:29:34 -0600

    Clear and decimal
    Added commands for clear and decimal buttons

--- Calculator/ViewModel/MainWindowViewModel.cs
+++ Calculator/ViewModel/MainWindowViewModel.cs
@@ -12,8 +12,10 @@ namespace Calculator.ViewModel
 		public ObservableCollection<OperatorButtonViewModel> OperatorButtons { get; set; }
 		public EqualButtonViewModel EqualButton { get; set; }
 
-		public RelayCommand NumericButtonCommand => new RelayCommand(execute => AddInputToString());
+		public RelayCommand NumericButtonCommand => new RelayCommand(execute => HandleNumericInput());
 		public RelayCommand OperatorButtonCommand => new RelayCommand(execute => HandleOperatorInput());
+		public RelayCommand DecimalButtonCommand => new RelayCommand(execute => HandleDecimalInput());
+		public RelayCommand ClearInputTextCommand => new RelayCommand(execute => ClearInputText());
 		public RelayCommand EqualButtonCommand => new RelayCommand(execute => ResolveExpression());
 
 		private List<string> _inputText = new List<string>();
@@ -34,8 +36,8 @@ namespace Calculator.ViewModel
 
 			OperatorButtons = new ObservableCollection<OperatorButtonViewModel>
 			{
-				new OperatorButtonViewModel("btnDecimal", ".", OperatorButtonCommand),
-				new OperatorButtonViewModel("btnClear", "Clr", OperatorButtonCommand),
+				new OperatorButtonViewModel("btnDecimal", ".", DecimalButtonCommand),
+				new OperatorButtonViewModel("btnClear", "Clr", ClearInputTextCommand),
 				new OperatorButtonViewModel("btnAdd", "+", OperatorButtonCommand),
 				new OperatorButtonViewModel("btnSubtract", "-", OperatorButtonCommand)
 			};
@@ -43,14 +45,22 @@ namespace Calculator.ViewModel
 			EqualButton = new EqualButtonViewModel("btnEqual", "=", EqualButtonCommand);
 		}
 
-		private void AddInputToString()
+		private void HandleNumericInput()
 		{
 			//Add the value of the button to the current list index
 			//Index will be incremented when an operator is pressed or entered
 		}
 		private void HandleOperatorInput()
 		{
-			//Add/Subtract, Clr, Decimal
+			//Add/Subtract
+		}
+		private void HandleDecimalInput()
+		{
+			//Decimal
+		}
+		private void ClearInputText()
+		{
+			//Clear
 		}
 		private void ResolveExpression()
 		{
